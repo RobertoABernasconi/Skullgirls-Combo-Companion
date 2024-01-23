818f133c7407f1fd30a4a4db4c880336156fcc01
Roberto Alfonso Bernasconi <roberto.a.bernasconi@gmail.com>
2024-01-23 19:56:07 +0100

    onClickListener per cambio di Activity

---
+++ app/src/main/java/com/example/skullgirlscombocompanion/ComboActivity.kt
@@ -0,0 +1,15 @@
+package com.example.skullgirlscombocompanion
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+
+class ComboActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val character: String? = intent.getStringExtra("character")
+
+        setContentView(R.layout.combo_view)
+        Toast.makeText(this, character, Toast.LENGTH_SHORT).show()
+    }
+}
--- app/src/main/java/com/example/skullgirlscombocompanion/MainActivity.kt
+++ app/src/main/java/com/example/skullgirlscombocompanion/MainActivity.kt
@@ -1,13 +1,57 @@
 package com.example.skullgirlscombocompanion
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
 import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val listener = View.OnClickListener { view ->
+            var intent = Intent(this@MainActivity, ComboActivity::class.java)
+            intent.putExtra("character", view.contentDescription)
+            startActivity(intent)
+        }
+
         setContentView(R.layout.activity_main)
 
+        val imageButton0 = findViewById<ImageButton>(R.id.imageButton1)
+        imageButton0.setOnClickListener(listener)
+        val imageButton1 = findViewById<ImageButton>(R.id.imageButton2)
+        imageButton1.setOnClickListener(listener)
+        val imageButton2 = findViewById<ImageButton>(R.id.imageButton3)
+        imageButton2.setOnClickListener(listener)
+        val imageButton3 = findViewById<ImageButton>(R.id.imageButton4)
+        imageButton3.setOnClickListener(listener)
+        val imageButton4 = findViewById<ImageButton>(R.id.imageButton5)
+        imageButton4.setOnClickListener(listener)
+        val imageButton5 = findViewById<ImageButton>(R.id.imageButton6)
+        imageButton5.setOnClickListener(listener)
+        val imageButton6 = findViewById<ImageButton>(R.id.imageButton7)
+        imageButton6.setOnClickListener(listener)
+        val imageButton7 = findViewById<ImageButton>(R.id.imageButton8)
+        imageButton7.setOnClickListener(listener)
+        val imageButton8 = findViewById<ImageButton>(R.id.imageButton9)
+        imageButton8.setOnClickListener(listener)
+        val imageButton9 = findViewById<ImageButton>(R.id.imageButton10)
+        imageButton9.setOnClickListener(listener)
+        val imageButton10 = findViewById<ImageButton>(R.id.imageButton11)
+        imageButton10.setOnClickListener(listener)
+        val imageButton11 = findViewById<ImageButton>(R.id.imageButton12)
+        imageButton11.setOnClickListener(listener)
+        val imageButton12 = findViewById<ImageButton>(R.id.imageButton13)
+        imageButton12.setOnClickListener(listener)
+        val imageButton13 = findViewById<ImageButton>(R.id.imageButton14)
+        imageButton13.setOnClickListener(listener)
+        val imageButton14 = findViewById<ImageButton>(R.id.imageButton15)
+        imageButton14.setOnClickListener(listener)
+        val imageButton15 = findViewById<ImageButton>(R.id.imageButton16)
+        imageButton15.setOnClickListener(listener)
+        val imageButton16 = findViewById<ImageButton>(R.id.imageButton17)
+        imageButton16.setOnClickListener(listener)
     }
+
 }
